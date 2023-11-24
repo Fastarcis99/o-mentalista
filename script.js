ac427c4259348a1873fee19bb4149497cd64fef0
Fastarcis99 <andersonvlima01@gmail.com>
2023-11-24 00:22:37 -0300

    docs: update script.js for adjust the last chance and style.css the container

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var secretNumber = parseInt(Math.random() * 1000 + 1); //Randomizar a escolha dos Números
 var currentAttempt = 10; //Número máximo de tentativas
-
+console.log(secretNumber)
 //Função do botão Chutar-----------------------------------
 function Chutar() {
   var resultado = document.getElementById("resultado"); // Correção: passar o ID correto
@@ -12,48 +12,52 @@ function Chutar() {
   }
   //Acabou as tentativas
   if (currentAttempt === 1) {
-    resultado.innerHTML =
-      "Que pena! Mais sorte da próxima vez. \nO número secreto era: " +
-      secretNumber;
-    //Resetar();
-    return;
-  }
-
-  if (chute == secretNumber) {
-    resultado.innerHTML = "Parabéns você acertou!";
-  } else if (chute > 1000 || chute < 1) {
-    resultado.innerHTML = "Ops! Escolha um número entre 1 e 1000";
-  } else if (chute > secretNumber) {
-    currentAttempt--;
-    resultado.innerHTML =
-      "Você errou! O número secreto é menor, que " +
-      chute +
-      ", agora você tem " +
-      currentAttempt +
-      " tentativas.";
-  } else if (chute < secretNumber) {
-    currentAttempt--;
-    resultado.innerHTML =
-      "Você errou! O número secreto é maior que " +
-      chute +
-      ", agora você tem " +
-      currentAttempt +
-      " tentativas.";
-  }
-  document.getElementById("valor").value = "";
-}
-
-document.getElementById("valor").addEventListener("keypress", function (event) {
-  if (event.key === "Enter") {
-    if (currentAttempt == 1) {
-      setTimeout(() => {
-        Resetar();
-      }, 5000);
+    if (chute == secretNumber) {
+      resultado.innerHTML =
+        "Parabéns! Você acertou!"
+      //Resetar();
+      return;
+    } else {
+      resultado.innerHTML =
+        "Que pena! Mais sorte da próxima vez! \nO número secreto era" +
+        secretNumber;
     }
-    Chutar();
   }
-});
+    if (chute == secretNumber) {
+      resultado.innerHTML = "Parabéns você acertou!";
+    } else if (chute > 1000 || chute < 1) {
+      resultado.innerHTML = "Ops! Escolha um número entre 1 e 1000";
+    } else if (chute > secretNumber) {
+      currentAttempt--;
+      resultado.innerHTML =
+        "Você errou! O número secreto é menor, que " +
+        chute +
+        ", agora você tem " +
+        currentAttempt +
+        " tentativas.";
+    } else if (chute < secretNumber) {
+      currentAttempt--;
+      resultado.innerHTML =
+        "Você errou! O número secreto é maior que " +
+        chute +
+        ", agora você tem " +
+        currentAttempt +
+        " tentativas.";
+    }
+    document.getElementById("valor").value = "";
+  }
 
-function Resetar() {
-  document.location.reload(false);
-}
+  document.getElementById("valor").addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      if (currentAttempt == 1) {
+        setTimeout(() => {
+          Resetar();
+        }, 5000);
+      }
+      Chutar();
+    }
+  });
+
+  function Resetar() {
+    document.location.reload(false);
+  }
